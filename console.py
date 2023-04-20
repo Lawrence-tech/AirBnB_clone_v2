0415cfac363b6e86eae73dca694c320ebd6362a1
Katemwende <kmwende419@gmail.com>
2023-04-20 08:29:52 +0000

    Console changes

--- console.py
+++ console.py
@@ -144,9 +144,8 @@ class HBNBCommand(cmd.Cmd):
                 setattr(new_instance, k, v)
             except ValueError:
                 continue
-        storage.save()
+        new_instance.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
