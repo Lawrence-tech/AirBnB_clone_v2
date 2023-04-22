0084fb160995a8dd999b8637cf5eaf7502f4cb66
Lawrence.tech <lawrencesagini2@gmail.com>
2023-04-22 15:43:25 -0700

    Revert "fix errors"
    This reverts commit 1911809b13cd16b110405583d4ece1d44fe1b280.

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import models
 from uuid import uuid4
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime, create_engine
-from os import environ
+from sqlalchemy import Column, Integer, String, DateTime
 
-storage_engine = environ.get("HBNB_TYPE_STORAGE")
-
-if (storage_engine == "db"):
-    Base = declarative_base()
-else:
-    Base = object
+Base = declarative_base()
 
 
 class BaseModel:
