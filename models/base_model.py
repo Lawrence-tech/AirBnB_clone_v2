1911809b13cd16b110405583d4ece1d44fe1b280
Lawrence.tech <lawrencesagini2@gmail.com>
2023-04-22 15:14:58 -0700

    fix errors

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+import models
 from uuid import uuid4
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, create_engine
+from os import environ
 
-Base = declarative_base()
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
+
+if (storage_engine == "db"):
+    Base = declarative_base()
+else:
+    Base = object
 
 
 class BaseModel:
