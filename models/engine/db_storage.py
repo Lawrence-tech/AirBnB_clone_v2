0bb5bb46e6f0f769929fabfd7fae5d779c1bea1f
Lawrence-tech <lawrencesagini2@gmail.com>
2023-05-21 19:55:51 +0000

    modified db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Defines the db storage engine"""
+"""Defines the database storage engine"""
+
 from os import getenv
-from models.base_model import BaseModel
-from models.base_model import Base
+from models.base_model import BaseModel, Base
 from models.user import User
 from models.place import Place
 from models.state import State
@@ -10,16 +10,16 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 from sqlalchemy import create_engine
-from sqlalchemy.orm import relationship
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship, scoped_session, sessionmaker
 
 
 class DBStorage:
-    """Represents a database storage engine.
+    """
+    Represents the database storage engine.
+
     Attributes:
-        __engine (sqlalchemy.Engine): The working SQLAlchemy engine.
-        __session (sqlalchemy.Session): The working SQLAlchemy session.
+        __engine (sqlalchemy.Engine): The SQLAlchemy engine.
+        __session (sqlalchemy.Session): The SQLAlchemy session.
     """
 
     __engine = None
@@ -37,10 +37,13 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Query on the curret database session all objects of the given class.
+        """
+        Query the current database session for objects of the given class.
+
         If cls is None, queries all types of objects.
-        Return:
-            Dict of queried classes in the format <class name>.<obj id> = obj.
+
+        Returns:
+            A dictionary of queried classes in the format <class name>.<obj id> = obj.
         """
         if cls is None:
             objs = self.__session.query(State).all()
@@ -56,7 +59,7 @@ class DBStorage:
         return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
 
     def new(self, obj):
-        """Add obj to the current database session."""
+        """Add the object to the current database session."""
         self.__session.add(obj)
 
     def save(self):
@@ -64,15 +67,14 @@ class DBStorage:
         self.__session.commit()
 
     def delete(self, obj=None):
-        """Delete obj from the current database session."""
+        """Delete the object from the current database session."""
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
         """Create all tables in the database and initialize a new session."""
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine,
-                                       expire_on_commit=False)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
 
