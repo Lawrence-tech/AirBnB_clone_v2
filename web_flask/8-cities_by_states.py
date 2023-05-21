5c5bcbf3f319a344e2c0e69a2d3529550c0c18e8
Lawrence-tech <lawrencesagini2@gmail.com>
2023-05-21 20:18:42 +0000

    9. Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-"""A Flask Web App for displaying cities by states"""
+"""Importing Flask to run the web app"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
 
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close(self):
-    """Close the current session"""
+def close_storage(exception):
+    """Method to remove current SQLAlchemy Session"""
     storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Display a HTML page with states and cities"""
+    """Displays an HTML page with states and cities"""
     states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=states)
+    cities = storage.all(City)
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    sorted_cities = sorted(cities.values(), key=lambda city: city.name)
+    return render_template('8-cities_by_states.html',
+                           states=sorted_states, cities=sorted_cities)
 
 
 if __name__ == '__main__':
